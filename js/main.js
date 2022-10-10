f4939026530ba339ce8763716d2abc01aa6535b7
hitotsu1 <payback1231@bk.ru>
2022-10-10 22:49:48 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,117 @@
+const navbar = document.querySelector(".navbar");
+const logoLigth = document.querySelector(".logo-light");
+const logo = document.querySelector("logo");
+const mMenuToggle = document.querySelector(".mobile-menu-toggle");
+const menu = document.querySelector(".mobile-menu");
+const isFront = document.body.classList.contains("front-page");
+
+
+  const lightModeOn = (event) => {
+   navbar.classList.add("navbar-light");
+  };
+  const lightModeOff = (event) => {
+   navbar.classList.remove("navbar-light");
+  };
+
+  const changeNavHeight = (height) => {
+    navbar.style.height = height;
+  }; 
+
+const openMenu = (event) => {
+  menu.classList.add("is-open");
+  mMenuToggle.classList.add("close-menu");
+  document.body.style.overflow = "hidden";
+  lightModeOn();
+};
+const closeMenu = (event) => {
+  menu.classList.remove("is-open");
+  mMenuToggle.classList.remove("close-menu");
+  document.body.style.overflow = "";
+  lightModeOff();
+};
+window.addEventListener("scroll", () => {
+  this.scrollY > 1 ? changeNavHeight('4.5rem') : changeNavHeight("5.875rem");
+  if (isFront) {
+    this.scrollY > 1 ? lightModeOn() : lightModeOff();
+  }    
+});
+mMenuToggle.addEventListener("click", (event) => {
+  event.preventDefault();
+  menu.classList.contains("is-open") ? closeMenu() : openMenu();
+}); 
+
+const swiperSteps = new Swiper(".steps-slider", {
+  speed: 400,
+  slidesPerView: 1,
+  navigation: {
+    nextEl: '.steps-button-next',
+    prevEl: '.steps-button-prev',
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    576: {
+      slidesPerView: 2,
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 3,
+    },
+    // when window width is >= 1024px
+    1024: {
+      slidesPerView: 4,
+    },
+  },
+});
+
+const swiper = new Swiper('.features-slider', {
+  speed: 400,
+  slidesPerView: 1,
+  navigation: {
+    nextEl: '.slider-button-next',
+    prevEl: '.slider-button-prev',
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    576: {
+      slidesPerView: 2,
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 3,
+    },
+    // when window width is >= 1024px
+    1024: {
+      slidesPerView: 4,
+    },
+  }
+});
+
+const swiperBlog = new Swiper(".blog-slider",{
+  speed: 400,
+  slidesPerView: 2,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.blog-button-next',
+    prevEl: '.blog-button-prev',
+  },
+});
+
+const modal = document.querySelector(".modal");
+const modalDialog = document.querySelector(".modal-dialog");
+
+document.addEventListener("click", (event) =>{
+  if (
+    event.target.dataset.toggle == "modal" ||
+    event.target.parentNode.dataset.toggle == "modal" ||
+    (!event.composedPath().includes(modalDialog) &&
+    modal.classList.contains("is-open"))
+  ) {
+    event.preventDefault();
+    modal.classList.toggle("is-open");
+  }
+});
+document.addEventListener("keyup", (event) => {
+  if (event.key == "Escape" && modal.classList.contains("is-open")) {
+    modal.classList.toggle("is-open");
+  }
+})  
